d1a140932f433a06b0ef7771662a42eb82cd3840
thangbui21 <thangbui2129@gmail.com>
2020-09-10 23:55:05 +0700

    Analytics

---
+++ src/pages/Analytics/Breakdown/Breakdown.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Breakdown",
+  data() {
+    return {
+      series: [44, 55, 41, 17, 15],
+          chartOptions: {
+            chart: {
+              type: 'donut',
+            },
+            responsive: [{
+              breakpoint: 480,
+              options: {
+                chart: {
+                  width: 200
+                },
+                legend: {
+                  position: 'bottom'
+                }
+              }
+            }]
+          },
+    };
+  },
+  
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ let publicPath = process.env.NODE_ENV === 'production' ? 'light-blue-vue-admin/'
 module.exports = {
   publicPath,
   productionSourceMap: false,
+
+  //Phải tạo proxy.
   devServer: {
     proxy: 'http://localhost:8080'
   }
